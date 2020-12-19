bd196c026c3dad5485ca7977c5716ad47d91eb9c
Shiprak4thJuly2007 <74818577+Shiprak4thJuly2007@users.noreply.github.com>
2020-12-19 20:07:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var car, wall, weight, speed;
+function setup() {
+  createCanvas(1600,400);
+speed=random(5,10)
+ weight=random(400,1500) 
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
